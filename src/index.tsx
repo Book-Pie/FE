dc0088fb4710d38fee7a0155a4d755081a823c73
Minwook Lee <dnr14@naver.com>
2021-12-17 17:18:05 +0900

    feat : pr test

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
+console.log("pr을 날려보자");
 
 ReactDOM.render(<App />, rootElement);
