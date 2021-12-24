ca202ae302520735311506dc5e476ccb3fa8a400
Woo Jieum <oozeume@gmail.com>
2021-12-24 16:41:58 +0900

    feature : mainpage - header

---
+++ src/components/Header.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Header = () => {
+  const [searchInput, setSearchInput] = useState<string>("");
+
+  const handleKeyPress = (e: any) => {
+    if (e.key === "Enter") {
+      console.log("onSearch");
+    }
+  };
+  return (
+    <HeaderContainer>
+      <h1>LOGO</h1>
+      <RouterWrapper>
+        <h2>중고장터</h2>
+        <h2>리뷰</h2>
+        <h2>커뮤니티</h2>
+      </RouterWrapper>
+      <form>
+        <input
+          type="text"
+          placeholder="search"
+          value={searchInput}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setSearchInput(e.target.value);
+          }}
+          onKeyPress={handleKeyPress}
+        />
+      </form>
+      <SignWrapper>
+        <h3>회원가입</h3>
+        <h3>로그인</h3>
+      </SignWrapper>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
+
+const HeaderContainer = styled.div`
+  background-color: pink;
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+`;
+
+const RouterWrapper = styled.div`
+  display: flex;
+  h2 {
+    padding-right: 10px;
+  }
+`;
+
+const SignWrapper = styled.div`
+  display: flex;
+  h3 {
+    padding-right: 10px;
+  }
+`;
---
+++ src/pages/SearchResult/SearchResult.tsx
@@ -0,0 +1,5 @@
+const SearchResult = () => {
+  return <div>검색결과페이지</div>;
+};
+
+export default SearchResult;
