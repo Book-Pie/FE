959bda0709bb07681ea7f27231ce8ef41cb70c32
Woo Jieum <oozeume@gmail.com>
2021-12-26 23:52:15 +0900

    feature : mainpage

--- src/pages/Main/Mainpage.tsx
+++ src/pages/Main/Mainpage.tsx
@@ -1,4 +1,3 @@
-import Footer from "src/components/Main/Footer";
 import ThumbnailSlider from "src/components/Main/ThumbnailSlider";
 import styled from "styled-components";
 
@@ -6,20 +5,11 @@ import BannerSlider from "../../components/Main/BannerSlider";
 
 const Mainpage = () => {
   return (
-    <>
-      <Container>
-        <BannerSlider />
-        <h3>중고상품추천</h3>
-        <ThumbnailSlider />
-        <h3>베스트셀러</h3>
-        <ThumbnailSlider />
-        <h3>인기 태그</h3>
-        <ThumbnailSlider />
-        <h3>최신 등록 상품</h3>
-        <ThumbnailSlider />
-      </Container>
-      <Footer />
-    </>
+    <Container>
+      <BannerSlider />
+      <h3>중고상품추천</h3>
+      <ThumbnailSlider />
+    </Container>
   );
 };
 
