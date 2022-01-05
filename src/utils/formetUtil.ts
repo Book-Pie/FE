aeb9797f08ee0294ee6e4d7adbdadcb9c64b3560
Minwook Lee <dnr14@naver.com>
2022-01-05 18:55:08 +0900

    feat : 핸드폰번호에 Hyphen 붙여주는 유틸 개발

--- src/utils/formetUtil.ts
+++ src/utils/formetUtil.ts
@@ -10,3 +10,18 @@ export const make1000UnitsCommaFormet = (text: string) => {
 export const hyphenRemoveFormat = (text: string) => {
   return text.replaceAll("-", "");
 };
+
+export const addHyphenFormat = (text: string) => {
+  const { length } = text;
+  return text.split("").reduce((acc, cur, idx) => {
+    const hyphenText = `${acc}-${cur}`;
+
+    if (idx === 3) return hyphenText;
+    // 010-0000-0000 11자
+    if (length === 11 && idx === 7) return hyphenText;
+    // 010-000-0000 10자
+    if (length === 10 && idx === 6) return hyphenText;
+
+    return acc + cur;
+  });
+};
